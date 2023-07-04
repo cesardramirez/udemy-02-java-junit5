72d3e5bec395117055bc242d4338d6d46f0fbd55
Cesar David Ramírez <cesardavid89@gmail.com>
2023-07-03 22:11:35 -0500

    feat(initial): Initial maven project. (#1)

---
+++ src/test/java/com/udemy/MainTest.java
@@ -0,0 +1,18 @@
+package com.udemy;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * A simple unit test
+ */
+public class MainTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
